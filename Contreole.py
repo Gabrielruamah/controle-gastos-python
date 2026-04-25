af3c9bbc6c2da19acc1cb54d3d812892631debbd
Gabriel Santana <kodak24252731@gmail.com>
2026-04-25 12:41:28 -0300

    Add files via upload

---
+++ Contreole.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+gasto1 = float(input("Digite seu primeiro gasto: "))
+gasto2 = float(input("Digite seu segundo gasto: "))
+
+total = gasto1 + gasto2
+
+print(f"{nome}, seu total de gastos é: R$ {total}")
